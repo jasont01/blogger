6cfb1de49deee34e7385e0f03baa8f3f4ad9b99e
Jason Thompson <jason@jasont.us>
2020-05-30 15:32:24 -0400

    add auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,51 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  before_action :require_login, except: [:index, :show]
+
   def index
     @articles = Article.all
   end
-
+  
   def show
     @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
   end
-
+  
   def new
     @article = Article.new
   end
-
+  
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article successfully created"
+      redirect_to @article
+    else
+      flash.now[:error] = "Something went wrong"
+      render 'new'
+    end
   end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    @article.update(article_params)
-    flash[:notice] = "Article was successfully updated"
-    redirect_to article_path(@article)
-  end
-
+  
   def destroy
     @article = Article.find(params[:id])
     @article.destroy ? flash[:notice] = 'Article was successfully deleted.' : flash[:error] = 'Something went wrong'
     redirect_to articles_url
   end
-
-  private
-
-  def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+  
+  def edit
+    @article = Article.find(params[:id])
   end
+  
+  def update
+    @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to @article
+      else
+        flash.now[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,12 @@
 class CommentsController < ApplicationController
+  include CommentsHelper
+  before_action :require_login, except: [:create]
+  
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
-  
-    @comment.save
-  
-    redirect_to article_path(@comment.article)
-  end
 
-  private
-  
-  def comment_params
-    params.require(:comment).permit(:author_name, :body)
+    @comment.save ? flash[:notice] = "Comment successfully created" : flash[:error] = "Something went wrong"
+    redirect_to article_path(@comment.article)
   end
 end
